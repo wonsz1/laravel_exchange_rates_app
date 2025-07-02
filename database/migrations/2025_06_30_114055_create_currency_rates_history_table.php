c341037cced4eba7f9e895507af4836471dde607
Mateusz Rusiecki <mateuszr87@gmail.com>
2025-07-02 22:18:16 +0200

    Update currency_rates_history table and use int values for rates instead of decimals

--- database/migrations/2025_06_30_114055_create_currency_rates_history_table.php
+++ database/migrations/2025_06_30_114055_create_currency_rates_history_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class () extends Migration {
     /**
      * Run the migrations.
      */
@@ -15,7 +14,7 @@ return new class extends Migration
             $table->id();
             $table->foreignId('from_currency_id')->constrained('currencies')->onDelete('cascade');
             $table->foreignId('to_currency_id')->constrained('currencies')->onDelete('cascade');
-            $table->decimal('rate', 10, 4);
+            $table->unsignedInteger('rate');
             $table->timestamp('date')->index();
             $table->timestamps();
 
